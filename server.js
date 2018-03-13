b7e92fbb66710f9923b200384a4b4d909641d99c
Yoeri Pasmans <yoerip@live.nl>
2018-03-13 14:24:00 +0100

    Added compression

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const nunjucks = require('nunjucks');
 const path = require('path');
 const routeStatic = require('./lib/route-static');
 const redirectIndices = require('./lib/redirect-indices');
+const compression = require('compression')
 
 const app = express();
+// compress all responses
+app.use(compression());
 const baseDir = 'src';
 const port = process.env.PORT || 3004;
 
@@ -17,6 +20,7 @@ app.use('/static', express.static(path.join(__dirname, baseDir), { etag: false, 
 
 // dynamic pages
 app.use(redirectIndices);
+
 nunjucks.configure(baseDir, {
     autoescape: true,
     express: app,
@@ -30,4 +34,3 @@ app.get('*', (req, res, next) => {
 app.listen(port, (err) => {
     err ? console.error(err) : console.log(`app running on http://localhost:${port}`);
 });
-
